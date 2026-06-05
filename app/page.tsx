d55883aa52c4f6f1e46f878d73ff6b448b6f16f0
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-05 16:05:49 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const MOCK_LEAGUES: League[] = [
   { id: 1, name: 'FIFA World Cup 2026', slug: 'fifa-world-cup-2026', logo: 'https://api.fifa.com/api/v3/picture/tournaments-sq-5/255711_w' },
-  { id: 2, name: 'English Premier League', slug: 'epl', logo: 'https://logos-world.net/wp-content/uploads/2020/06/Premier-League-Logo.png' },
-  { id: 3, name: 'La Liga', slug: 'la-liga', logo: 'https://logos-world.net/wp-content/uploads/2020/06/LaLiga-Logo.png' },
-  { id: 4, name: 'Serie A', slug: 'serie-a', logo: 'https://logos-world.net/wp-content/uploads/2020/06/Serie-A-Logo.png' },
-  { id: 5, name: 'Bundesliga', slug: 'bundesliga', logo: 'https://logos-world.net/wp-content/uploads/2020/06/Bundesliga-Logo.png' },
-  { id: 6, name: 'UEFA Champions League', slug: 'uefa-champions-league', logo: 'https://logos-world.net/wp-content/uploads/2020/06/UEFA-Champions-League-Logo.png' },
+  { id: 2, name: 'UEFA Champions League', slug: 'uefa-champions-league', logo: 'https://img.uefa.com/imgml/uefacom/ucl/2024/logos/logo_dark.svg' },
+  { id: 3, name: 'English Premier League', slug: 'epl', logo: 'https://resources.premierleague.com/premierleague/badges/t1.png' },
+  { id: 4, name: 'La Liga', slug: 'la-liga', logo: 'https://assets.laliga.com/assets/logos/laliga-v-negro/laliga-v-negro-300x300.png' },
+  { id: 5, name: 'Serie A', slug: 'serie-a', logo: 'https://www.legaseriea.it/assets/logo-serie-a-2024.svg' },
+  { id: 6, name: 'Bundesliga', slug: 'bundesliga', logo: 'https://www.bundesliga.com/assets/logo/bundesliga-logo-2021.svg' },
 ]
