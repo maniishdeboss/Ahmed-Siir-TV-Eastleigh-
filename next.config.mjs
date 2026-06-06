8389ea3edf95b608309afaa0e33a17dc77618903
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-06 16:57:10 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '4dar91ffoetct2nu.public.blob.vercel-storage.com',
+      },
+    ],
   },
 }
+
+export default nextConfig
