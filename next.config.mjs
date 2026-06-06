183db4626d0141b9f693e84a20c8362c0274f063
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-06 16:50:55 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
-
-export default nextConfig
