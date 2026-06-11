ed51a1ebb3add9fd9e885da7691891c1d794c0cd
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-11 23:22:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script' 
 
 export const metadata: Metadata = {
   title: 'Sports Live',
@@ -13,6 +14,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Kani waa koodka OneSignal ee ogeysiisyada kuu keenaya */}
+        <Script 
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" 
+          defer 
+        />
+        <Script id="onesignal-init" strategy="afterInteractive">
+          {`
+            window.OneSignal = window.OneSignal || [];
+            OneSignal.push(function() {
+              OneSignal.init({
+                appId: "D5e7c99a-391f-4191-b811-e2b199450cfa",
+              });
+            });
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
