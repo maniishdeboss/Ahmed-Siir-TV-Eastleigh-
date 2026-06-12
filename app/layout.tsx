0184faf7562c1b2c2fc203ba4d5e09a01fd40c43
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-12 21:16:31 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import './globals.css';
+import OneSignalInit from '../components/OneSignalInit'; // Halkan ayaan kaga soo yeernay faylkaaga cusub
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <OneSignalInit /> 
         {children}
       </body>
     </html>
