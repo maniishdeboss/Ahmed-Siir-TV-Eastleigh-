c56eca13c061b34a4dad70148f1283673a4a8a05
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-12 21:21:44 +0300

    Update OneSignalInit.tsx

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -1,12 +1,19 @@
-'use client'; // Tani waa qaybta ugu muhiimsan
+'use client';
 
 import { useEffect } from 'react';
 import Script from 'next/script';
 
+// Kudar koodkan yar si TypeScript u aqoonsado OneSignal
+declare global {
+  interface Window {
+    OneSignalDeferred: any;
+  }
+}
+
 export default function OneSignalInit() {
   useEffect(() => {
     window.OneSignalDeferred = window.OneSignalDeferred || [];
-    window.OneSignalDeferred.push(async function(OneSignal) {
+    window.OneSignalDeferred.push(async function(OneSignal: any) {
       await OneSignal.init({
         appId: "d5e7c99a-391f-4191-b811-e2b199450cfa",
         notifyButton: {
