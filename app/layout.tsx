a0915a6fd31b48102274b02712124f8a362682c4
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-12 13:59:57 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next'
-import './globals.css'
 import Script from 'next/script'
-import SubscribeButton from './SubscribeButton' // Halkan ayaan kaga soo wacnay faylkii cusbaa
 
-export const metadata: Metadata = {
-  title: 'Sports Live',
-  description: 'Watch live sports streams',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -26,16 +15,16 @@ export default function RootLayout({
             window.OneSignalDeferred.push(async function(OneSignal) {
               await OneSignal.init({
                 appId: "d5e7c99a-391f-4191-b811-e2b199450cfa",
-                safari_web_id: "web.onesignal.auto.694eada3-a476-4a33-8c61-99aa6c1d60b0",
+                // Tani waxay soo saaraysaa badhan gambaleel ah oo ka caawinaya qofka inuu subscribe-gareeyo
+                notifyButton: {
+                  enable: true,
+                },
               });
             });
           `}
         </Script>
       </head>
-      <body>
-        <SubscribeButton /> 
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
