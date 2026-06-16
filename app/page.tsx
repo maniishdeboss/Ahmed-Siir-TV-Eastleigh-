001b3db4b3257332a1abc30a88abef4fe1af37be
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-16 22:33:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ const SPORTS_CHANNELS = [
 ];
 
 const FEATURED_TV = [
-  { title: "SIIR TV", url: "https://a12.kooora-sia.com/bein-1/", bg: "from-blue-900 to-black" },
-  { title: "KOORA TV", url: "https://a12.kooora-sia.com/bein-1/", bg: "from-green-900 to-black" },
+  { title: "SIIR TV", url: "https://siiiiiiir.tv/hard/2908c7d4425d87350.html?match=4627862", bg: "from-blue-900 to-black" },
+  { title: "Siir Tv", Url: "https://siiiiiiir.tv/hard/2908c7d4425d87350.html?match=4627862," bg: "from-green-900 to-black" },
 ];
 
 const WATCH_BY_COUNTRY = [
