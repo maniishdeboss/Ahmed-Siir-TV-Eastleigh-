65f725726bbe6e0c91270c3956ce4d866563636b
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-21 00:08:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,8 @@ const WATCH_BY_COUNTRY = [
 
 // LEAGUES DATA
 const FOOTBALL_LEAGUES = [
-  { id: 1, name: "FIFA World Cup", logo: "🏆", bg: "bg-gradient-to-r from-blue-600 to-blue-800" },
-  { id: 2, name: "International Friendly", logo: "⚽", bg: "bg-gradient-to-r from-slate-700 to-slate-900" },
+  { id: 1, name: "FIFA World Cup", logo: "🇩🇪🏆🇨🇮", bg: "bg-gradient-to-r from-blue-600 to-blue-800" },
+  { id: 2, name: "International Friendly", logo: "🇦🇺⚽", bg: "bg-gradient-to-r from-slate-700 to-slate-900" },
   { id: 3, name: "English Premier League", logo: "🦁", bg: "bg-gradient-to-r from-purple-700 to-purple-900" },
   { id: 4, name: "Serie A", logo: "🇮🇹", bg: "bg-gradient-to-r from-blue-500 to-blue-700" },
   { id: 5, name: "La Liga", logo: "🇪🇸", bg: "bg-gradient-to-r from-red-600 to-yellow-500" },
@@ -44,7 +44,7 @@ const MATCHES_DATA: Record<number, any[]> = {
   1: [
     { 
       id: 1, 
-      team1: "Ahmed TV", 
+      team1: "Ahmed Abdikani TV", 
       team2: "World cup ", 
       flag1: "📺💥🇸🇴", 
       flag2: "🇸🇴🇦🇺", 
@@ -66,11 +66,11 @@ const MATCHES_DATA: Record<number, any[]> = {
     },
     { 
       id: 3, 
-      team1: "Scotland", 
-      team2: "Morocco", 
-      flag1: "🏴󠁧󠁢󠁳󠁣󠁴󠁿", 
-      flag2: "🇲🇦", 
-      time: "Ongoing 0-1 1:00", 
+      team1: "Germany", 
+      team2: "ivory coast", 
+      flag1: "🇩🇪 ", 
+      flag2: "🇨🇮", 
+      time: "Ongoing 0-1", 
       quality: "HD", 
       link: "https://siiiiiiir.tv/hard/2908c7d4425d87350.html?match=4627865",
       isYoutube: false
