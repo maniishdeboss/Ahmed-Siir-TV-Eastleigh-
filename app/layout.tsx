78bd599f30559ea8a153ee1c91221dd4a29e0909
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-27 22:35:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css';
 import OneSignalInit from '../components/OneSignalInit'; // Halkan ayaan kaga soo yeernay faylkaaga cusub
 
+// Tani waxay ku qasbaysaa telefoonada inay u furaan boggaga sidii desktop oo kale
+export const viewport = {
+  width: 1200, 
+  initialScale: 1,
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
