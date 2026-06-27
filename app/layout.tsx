1ba6d1de6b960f07ded878ac3fad61e81a7d2560
maniishdeboss <maniishwillboqortoyo@gmail.com>
2026-06-27 22:43:07 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css';
 import OneSignalInit from '../components/OneSignalInit'; // Halkan ayaan kaga soo yeernay faylkaaga cusub
 
-// Tani waxay ku qasbaysaa telefoonada inay u furaan boggaga sidii desktop oo kale
+// Tani waxay ku qasbaysaa telefoonada inay u furaan boggaga sidii desktop oo kale iyadoo dhan oo wada muuqata
 export const viewport = {
   width: 1200, 
-  initialScale: 1,
+  initialScale: 0.35,  // Bogga wuxuu furmayaa isagoo dib u raddan (zoomed out) si uu dhan u muuqdo
+  minimumScale: 0.35,  // Waxay u diadaysaa inuu isku dhoweeyo oo dacallada ka go'o
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
